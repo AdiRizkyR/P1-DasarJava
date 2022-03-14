46f1d5cd09ddd5289226cb6fd719c554cd8a77d4
AdiRizkyR <adirizkyramadhan912@gmail.com>
2022-03-14 16:33:08 +0700

    Percobaan Mencetak Tipe Default Dari Tipe Dasar di Java

---
+++ src/F_MencetakTipeDefaultDariTipeDasar.java
@@ -0,0 +1,21 @@
+public class F_MencetakTipeDefaultDariTipeDasar {
+    static boolean b;
+    static char c;
+    static byte bt;
+    static short s;
+    static int i;
+    static  long l;
+    static  float f;
+    static  double d;
+
+    public static void main(String[] args) {
+        System.out.println("Defaut value b = " + b);
+        System.out.println("Defaut value c = " + c);
+        System.out.println("Defaut value bt = " + bt);
+        System.out.println("Defaut value s = " + s);
+        System.out.println("Defaut value i = " + i);
+        System.out.println("Defaut value l = " + l);
+        System.out.println("Defaut value f = " + f);
+        System.out.println("Defaut value d = " + d);
+    }
+}
