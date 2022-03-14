7ff0df174a09674e3a04a1df094ffef8cdabe1cc
AdiRizkyR <adirizkyramadhan912@gmail.com>
2022-03-14 17:11:36 +0700

    Percobaan Mencetak Nilai Konstanta di Java

---
+++ src/MencetakNilaiKonstanta.java
@@ -0,0 +1,8 @@
+public class MencetakNilaiKonstanta {
+    public static void main(String[] args) {
+        final double PHI = 3.14;
+        final double CM_PER_INCH = 2.54;
+        System.out.println("nilai konstanta PHI = " + PHI);
+        System.out.println("nilai konstanta CM_PER_INCH = " + CM_PER_INCH);
+    }
+}
