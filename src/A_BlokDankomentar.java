af6a8ed35fcfdc3a7dfe373255af337d28772cf2
AdiRizkyR <adirizkyramadhan912@gmail.com>
2022-03-14 15:18:12 +0700

    Initial Project pertemuan 1 dasar java

---
+++ src/A_BlokDankomentar.java
@@ -0,0 +1,14 @@
+public class A_BlokDankomentar {
+    public static void main(String[] args) {
+        System.out.println("pemograman Berorientasi Objek dengan Java");
+        //contoh Komentar satu baris
+
+        /*
+        Contoh komentar libih dari satu baris
+         */
+
+        /**
+         * Java Doc --> bars ini untuk mendokumentasikan kode program
+         */
+    }
+}
