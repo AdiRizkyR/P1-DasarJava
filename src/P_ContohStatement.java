0a37da8d4f018063c5fa701b41b272d998de4871
AdiRizkyR <adirizkyramadhan912@gmail.com>
2022-03-14 20:30:47 +0700

    Percobaan contoh statement di Java

---
+++ src/P_ContohStatement.java
@@ -0,0 +1,10 @@
+public class P_ContohStatement {
+    public static void main(String[] args) {
+        System.out.println("semoga saya bisa Belajar");
+        System.out.println("Pemograman berorientasi Objek dengan java");
+        int angka = 10;
+        char charA = 'A';
+        System.out.println(angka);
+        System.out.println(charA);
+    }
+}
