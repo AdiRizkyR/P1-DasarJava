048d4f4e3b0113acd8e6d164529c38a7096b5397
AdiRizkyR <adirizkyramadhan912@gmail.com>
2022-03-14 20:27:30 +0700

    Percobaan Mencetak literal String di Java

---
+++ src/M_MencetakLiteralString.java
@@ -0,0 +1,6 @@
+public class M_MencetakLiteralString {
+    public static void main(String[] args) {
+        String welcome = "Pemograman berorientasi objek dengan java";
+        System.out.println(welcome);
+    }
+}
